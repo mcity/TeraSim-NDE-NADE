4e09f69fe742972b0c3b58d0cc90c0f3b3d7a49f
rajanikant.patnaika@gmail.com <rajanikant.patnaika@gmail.com>
2023-09-26 11:58:48 -0700

    For AWS deployment

--- safetest_mcity_cosim_main.py
+++ safetest_mcity_cosim_main.py
@@ -14,33 +14,42 @@ except:
 
 import argparse
 
+def main(args):
+
+    highlight_routes = set(["7", "10", "13", "14", "16"])
+    monitor = EnvMonitor(
+        highlight_routes=highlight_routes,
+        log_dir=f"{args.dir}/{args.mode}/raw_data",
+        exp_id=f"{args.mode}_{args.nth}",
+    )
+
+    veh_factory = MRVehicleFactory()
+    env = SafeTestNADECoSim(
+        vehicle_factory = veh_factory,
+        info_extractor=InfoExtractor,
+    )
+    sim = Simulator(
+        sumo_net_file_path = './maps/Mcity/mcity_new.net.xml',
+        sumo_config_file_path = './maps/Mcity/mcity_new.sumocfg',
+        num_tries=10,
+        gui_flag=False,
+        output_path=f"{args.dir}/{args.mode}/raw_data/{args.mode}_{args.nth}",
+        sumo_output_file_types=["fcd", "collision"],
+        realtime_flag=True,
+    )
+    monitor.bind_env(env)
+    sim.bind_env(env)
+    sim.run()
+
 parser = argparse.ArgumentParser(description='Run simulation.')
 parser.add_argument('--dir', type=str, help='output directory', default="output")
 parser.add_argument('--mode', type=str, help='the negligence mode.', default="test")
 parser.add_argument('--nth', type=str, help='the nth epoch', default="0_0")
 args = parser.parse_args()
+r = redis.Redis()
 
-highlight_routes = set(["7", "10", "13", "14", "16"])
-monitor = EnvMonitor(
-    highlight_routes=highlight_routes,
-    log_dir=f"{args.dir}/{args.mode}/raw_data",
-    exp_id=f"{args.mode}_{args.nth}",
-)
+while True:
+    r.flushdb()
+    main(args)
 
-veh_factory = MRVehicleFactory()
-env = SafeTestNADECoSim(
-    vehicle_factory = veh_factory,
-    info_extractor=InfoExtractor,
-)
-sim = Simulator(
-    sumo_net_file_path = './maps/Mcity/mcity_new.net.xml',
-    sumo_config_file_path = './maps/Mcity/mcity_new.sumocfg',
-    num_tries=10,
-    gui_flag=False,
-    output_path=f"{args.dir}/{args.mode}/raw_data/{args.mode}_{args.nth}",
-    sumo_output_file_types=["fcd", "collision"],
-    realtime_flag=True,
-)
-monitor.bind_env(env)
-sim.bind_env(env)
-sim.run()
+
