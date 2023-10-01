fcb687d1530d1fca6ac29f54f133b86bd9eb6c03
QZJGeorge <zhijieq@umich.edu>
2023-09-30 21:54:42 -0400

    remove everything

--- bash_files/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mode="ite"
-mkdir -p tfl_samples
-DIR_NAME=""
-for file in $(cat file.txt); do
-    cp -r ${DIR_NAME}/${file} tfl_samples
-done
--- examples/main_rosnode_av_tls.py
+++
@@ -1,7 +0,0 @@
-from terasim_mr.communicationtools.redis2ros import ROSPublisher_DummyTrafficlight,ROSPublisher_SUMOTrafficlight
-
-
-# Create the ROS publisher to publish the frontal traffic light signal information to ROS-based Autoware
-ros_pub = ROSPublisher_SUMOTrafficlight()
-# ros_pub = ROSPublisher_DummyTrafficlight()
-ros_pub.run()
