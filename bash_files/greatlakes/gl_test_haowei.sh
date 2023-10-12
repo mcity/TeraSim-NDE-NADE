9b8e684e8de5041cb9dbf01b2d2e10964d410ef8
Haowei Sun <printfplus@outlook.com>
2023-10-12 09:57:41 -0400

    modify gl test bash file

--- bash_files/greatlakes/gl_test_haowei.sh
+++ bash_files/greatlakes/gl_test_haowei.sh
@@ -25,29 +25,29 @@ source /home/haoweis/Safe-Test-TeraSim/venv/bin/activate
 DIR_NAME="/scratch/henryliu_root/henryliu98/shared_data/safetest-nade"
 export HAS_LIBSUMO=1
 
-mode="ITE_calibration_change_2roundabout_out_priority_change_map"
-mkdir -p ${DIR_NAME}/${mode}
-mkdir -p ${DIR_NAME}/${mode}/raw_data
-mkdir -p ${DIR_NAME}/${mode}/raw_data/final_state
-mkdir -p ${DIR_NAME}/${mode}/raw_data/maneuver_challenges
+exp_name="ITE_autoware_universe_map_recalibrate"
+mkdir -p ${DIR_NAME}/${exp_name}
+mkdir -p ${DIR_NAME}/${exp_name}/raw_data
+mkdir -p ${DIR_NAME}/${exp_name}/raw_data/final_state
+mkdir -p ${DIR_NAME}/${exp_name}/raw_data/maneuver_challenges
 
 del_mode="all"
 
 for i in {1..200}; do
-    mkdir -p ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}
+    mkdir -p ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}
     # test record
-    python safetest_mcity_main.py --dir ${DIR_NAME} --mode ${mode} --nth ${SLURM_ARRAY_TASK_ID}_${i} > ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}/res.txt
+    python applications/driving_intelligence_test/safetest_mcity_main.py --dir ${DIR_NAME} --exp_name ${exp_name} --nth ${SLURM_ARRAY_TASK_ID}_${i} > ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}/res.txt
     # remove if no collision with del_mode
-    if [ $(grep "collisions" ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}/collision.xml -m 1 | wc -l) -eq 0 ]; then
-        echo "Preserve ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}"
+    if [ $(grep "collisions" ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}/collision.xml -m 1 | wc -l) -eq 0 ]; then
+        echo "Preserve ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}"
     else
-        if [ $(grep "victim" ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}/collision.xml -m 1 | wc -l) -eq 0 ]; then
-            echo "Removing ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}"
+        if [ $(grep "victim" ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}/collision.xml -m 1 | wc -l) -eq 0 ]; then
+            echo "Removing ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}"
             # if del_mode is verbose, only remove fcd_all.xml
             if [ "${del_mode}" = "verbose" ]; then
-                rm -f ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}/fcd_all.xml
+                rm -f ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}/fcd_all.xml
             else
-                rm -rf ${DIR_NAME}/${mode}/raw_data/${mode}_${SLURM_ARRAY_TASK_ID}_${i}
+                rm -rf ${DIR_NAME}/${exp_name}/raw_data/${exp_name}_${SLURM_ARRAY_TASK_ID}_${i}
             fi
         fi
     fi
