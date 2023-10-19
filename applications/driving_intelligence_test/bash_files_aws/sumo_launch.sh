61ff9838d4e25744b8b401497b7329847d49818b
QZJGeorge <zhijieq@umich.edu>
2023-10-19 22:25:03 +0000

    fix aws second iteration error

--- applications/driving_intelligence_test/bash_files_aws/sumo_launch.sh
+++ applications/driving_intelligence_test/bash_files_aws/sumo_launch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-sleep 3
+sleep 2
 
-# temporary directory
 DIR_NAME="/home/ubuntu/terasim/TeraSim-NDE-ITE/applications/driving_intelligence_test/output"
 export HAS_LIBSUMO=1
 
 echo $HOSTNAME
 VERSION=v5
 mode=cosim_test_aws_${VERSION}_${HOSTNAME}
+
+i=$(</home/ubuntu/terasim/TeraSim-NDE-ITE/applications/driving_intelligence_test/iteration.txt)
+next=$((i+1))
+echo $next > /home/ubuntu/terasim/TeraSim-NDE-ITE/applications/driving_intelligence_test/iteration.txt
 
 mkdir -p ${DIR_NAME}/${mode}
 mkdir -p ${DIR_NAME}/${mode}/raw_data
@@ -18,25 +21,30 @@ del_mode="all"
 
 cd /home/ubuntu/terasim/TeraSim-NDE-ITE/applications/driving_intelligence_test
 
-for i in {1..1000}; do
-    mkdir -p ${DIR_NAME}/${mode}/raw_data/${mode}_0_${i}
-    mkdir -p ${DIR_NAME}/${mode}/raw_data/cav_context_0_${i}
-    
-    redis-cli set launch_autoware 1
-    redis-cli set iteration ${i}
-    redis-cli set hostname $HOSTNAME
-    redis-cli set version $VERSION
-    
-    echo "initializing autoware, waiting for 45 seconds..."
-    sleep 45
-    
-    python3 safetest_mcity_cosim_main.py --dir ${DIR_NAME} --name ${mode} --nth 0_${i}
+mkdir -p ${DIR_NAME}/${mode}/raw_data/${mode}_0_${i}
+mkdir -p ${DIR_NAME}/${mode}/raw_data/cav_context_0_${i}
 
-    redis-cli set launch_autoware 0
+redis-cli set iteration ${i}
+redis-cli set hostname $HOSTNAME
+redis-cli set version $VERSION
 
-    echo "shuting down autoware, waiting for 45 seconds..."
-    sleep 45
+echo "initializing autoware, waiting for 45 seconds..."
+sleep 45
 
-    echo "uploading file to aws cloud..."
-    python3 upload_output_aws.py --dir ${DIR_NAME} --mode ${mode} --nth 0_${i}
+python3 safetest_mcity_cosim_main.py --dir ${DIR_NAME} --name ${mode} --nth 0_${i}
+
+# Get a list of ROS2 process IDs and kill them
+pids=$(pgrep -f ros2)
+for pid in $pids
+do
+    kill -INT $pid
 done
+
+sleep 15
+
+echo "uploading file to aws cloud..."
+python3 upload_output_aws.py --dir ${DIR_NAME} --mode ${mode} --nth 0_${i}
+echo "file uploading complete"
+
+sleep 15
+sudo reboot
--- applications/driving_intelligence_test/bash_files_aws/sumo_record.sh
+++ applications/driving_intelligence_test/bash_files_aws/sumo_record.sh
@@ -2,16 +2,4 @@
 sleep 3
 
 cd /home/ubuntu/terasim/TeraSim-NDE-ITE/applications/driving_intelligence_test
-while true
-do
-    # Use redis-cli to get the value of the key. Change mykey to your key.
-    value=$(redis-cli get launch_autoware)
-    
-    if [[ $value -eq 1 ]]
-    then
-    	python3 sumo_record_aws.py
-    fi
-
-    # Wait for a while before checking again.
-    sleep 1
-done
+python3 sumo_record_aws.py
