08f299316eb1ccdc110662bb07dcfd35020e6031
Haowei Sun <printfplus@outlook.com>
2023-10-24 21:09:48 -0400

    update get AV distance total

--- analysis/get_distance_from_fcd.py
+++ analysis/get_distance_from_fcd.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xml.etree.ElementTree as ET
+from pathlib import Path
 
 def location_with_timestamp_extract_from_xml(fcd_root, vehicle_id):
     vehicle_locations = []
@@ -31,9 +32,13 @@ def get_distance_from_fcd(fcd_path, vehicle_id):
     fcd_root = ET.parse(fcd_path).getroot()
     locations = location_with_timestamp_extract_from_xml(fcd_root, vehicle_id)
     return get_vehicle_distance_from_location_with_timestamp(locations)
-
-# if __name__ == "__main__":
-#     fcd_file_name = "fcd_all.xml"
-#     vehicle_id = "CAV"
-#     locations = location_with_timestamp_extract_from_xml(fcd_file_name, vehicle_id)
-#     print(get_vehicle_distance_from_location_with_timestamp(locations))
+from tqdm import tqdm
+if __name__ == "__main__":
+    data_folder = Path("./applications/driving_intelligence_test/output/cosim_test_local/raw_data")
+    fcd_file_list = list(data_folder.glob("*/fcd.xml"))
+    total_distance = 0
+    for fcd_file_name in tqdm(fcd_file_list):
+        vehicle_id = "CAV"
+        # print(fcd_file_name, get_distance_from_fcd(fcd_file_name, vehicle_id))
+        total_distance += get_distance_from_fcd(fcd_file_name, vehicle_id)
+    print(total_distance)
