eafbcc89593a742c9935738b5f41ebd9eb8f3623
Haowei Sun <printfplus@outlook.com>
2023-10-28 21:28:33 -0400

    move safetest av to driving intelligence test

--- applications/driving_intelligence_test/safetest_mcity_main_AV.py
+++ applications/driving_intelligence_test/safetest_mcity_main_AV.py
@@ -1,3 +1,8 @@
+import sys
+import os
+file_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(file_path + "/../../")
+
 from terasim.simulator import Simulator
 from envs.env_monitor import EnvMonitor
 from envs.safetest_nde import SafeTestNDE
@@ -10,27 +15,27 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Run simulation.')
 parser.add_argument('--dir', type=str, help='output directory', default="output")
-parser.add_argument('--mode', type=str, help='the negligence mode.', default="test")
+parser.add_argument('--exp_name', type=str, help='experiment name', default="test")
 parser.add_argument('--nth', type=str, help='the nth epoch', default="0_0")
 args = parser.parse_args()
 
-highlight_routes = set(["7", "10", "13", "14", "16"])
+highlight_routes = {"7", "10", "13", "14", "16"}
 monitor = EnvMonitor(
     highlight_routes=highlight_routes,
-    log_dir=f"{args.dir}/{args.mode}/raw_data",
-    exp_id=f"{args.mode}_{args.nth}",
+    log_dir=f"{args.dir}/{args.exp_name}/raw_data",
+    exp_id=f"{args.exp_name}_{args.nth}",
 )
 env = SafeTestNADEWithAV(
     vehicle_factory = SafetestVehicleFactory(),
     info_extractor=InfoExtractor,
 )
 sim = Simulator(
-    sumo_net_file_path = './maps/Mcity/mcity_new.net.xml',
-    sumo_config_file_path = './maps/Mcity/mcity_new.sumocfg',
+    sumo_net_file_path = file_path + '/maps/Mcity_safetest/mcity.net.xml',
+    sumo_config_file_path = file_path + '/maps/Mcity_safetest/mcity.sumocfg',
     num_tries=10,
     gui_flag=True,
-    output_path=f"{args.dir}/{args.mode}/raw_data/{args.mode}_{args.nth}",
-    sumo_output_file_types=["fcd", "collision", "tripinfo"],
+    output_path=f"{args.dir}/{args.exp_name}/raw_data/{args.exp_name}_{args.nth}",
+    sumo_output_file_types=["fcd_all", "collision", "tripinfo"],
 )
 monitor.bind_env(env)
 sim.bind_env(env)
