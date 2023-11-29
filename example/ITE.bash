03aa762085147d131bff72b2088e67d9e18a7f57
Haowei Sun <printfplus@outlook.com>
2023-11-28 21:42:30 -0500

    delete safe trajectories

--- example/ITE.bash
+++ example/ITE.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 export USE_LIBSUMO="1"
 DIR_NAME="./output"
-
+del_mode="all"
 for i in {1..1000}; do
 
     experiment_name="ITE_test_$(date +%Y%m%d-%H%M%S)"
@@ -14,5 +14,12 @@ for i in {1..1000}; do
     mkdir -p ${DIR_NAME}/${experiment_name}/raw_data/
     # test record
     python3 safetest_mcity_main.py --dir ${DIR_NAME} --name ${experiment_name} --nth '0' > ${DIR_NAME}/${experiment_name}/raw_data/res.txt
-    # python3 data_upload.py --dir ${DIR_NAME} --name ${experiment_name} &
+    if [ $(grep "victim" ${DIR_NAME}/${experiment_name}/raw_data/${experiment_name}_0/collision.xml | wc -l) -eq 0 ]; then
+        echo "Removing ${DIR_NAME}/${experiment_name}"
+        if [ "${del_mode}" = "verbose" ]; then
+            rm -f ${DIR_NAME}/${experiment_name}/raw_data/${experiment_name}_0/fcd_all.xml
+        else
+            rm -rf ${DIR_NAME}/${experiment_name}
+        fi
+    fi
 done
