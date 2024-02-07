f9beb5c17599a2b8f1c7a8190277f238d9167377
Haowei Sun <printfplus@outlook.com>
2024-02-07 16:07:20 -0500

    NADE_IS_20_add_merge

--- example/gl_ITE.bash
+++ example/gl_ITE.bash
@@ -27,7 +27,7 @@ DIR_NAME="/scratch/henryliu_root/henryliu98/shared_data/safetest-nade"
 export USE_LIBSUMO=1
 
 # add time stamp to experiment name
-experiment_name="ITE_balance_cutin_collision_1000_IS_calibrate9_fix_NADE_IS_20_longer"
+experiment_name="NADE_IS_20_add_merge"
 mkdir -p ${DIR_NAME}/${experiment_name}
 mkdir -p ${DIR_NAME}/${experiment_name}/raw_data
 mkdir -p ${DIR_NAME}/${experiment_name}/raw_data/final_state
